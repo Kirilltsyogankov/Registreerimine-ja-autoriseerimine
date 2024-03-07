932916b2abc3722ea2aa47d6927ca618f17ba31c
opilane <opilane@HP-CZC2349HT4>
2024-03-07 15:51:03 +0200

    Add project files.

---
+++ Registreerimine ja autoriseerimine/Registreerimine_ja_autoriseerimine.py
@@ -0,0 +1,36 @@
+﻿import random
+import string
+
+def registreeri_kasutaja(nimi, parool, kasutajate_nimekiri):
+    if nimi in kasutajate_nimekiri:
+        print("Kasutajanimi on juba kasutusel!")
+        return False
+    kasutajate_nimekiri[nimi] = parool
+    print("Kasutaja registreeritud edukalt!")
+    return True
+
+def genereeri_parool(pikkus=8):
+    erimargid = ".,:;!_*-+"
+    numbrid = '0123456789'
+    vaiketahed = 'qwertyuiopasdfghjklzxcvbnm'
+    suurtahed = vaiketahed.upper()
+    koik_tahed = erimargid + numbrid + vaiketahed + suurtahed
+    tahed = list(koik_tahed)
+    random.shuffle(tahed)
+    parool = ''.join([random.valik(tahed) for x in range(pikkus)])
+    return parool
+
+def käsitsi_parool():
+    while True:
+        parool = input("Sisesta parool: ")
+        if len(parool) < 8:
+            print("Parool peab olema vähemalt 8 tähemärki pikk!")
+        if not any(taht.isdigit() for taht in parool):
+            print("Parool peab sisaldama vähemalt üht numbrit!")
+        if not any(taht.islower() for taht in parool):
+            print("Parool peab sisaldama vähemalt üht väiketähte!")
+        if not any(taht.isupper() for taht in parool):
+            print("Parool peab sisaldama vähemalt üht suurtähte!")
+        if not any(taht in string.punctuation for taht in parool):
+            print("Parool peab sisaldama vähemalt üht erimärki!")
+        return parool
---
+++ Registreerimine ja autoriseerimine/module1.py
@@ -0,0 +1,59 @@
+﻿import module1
+
+def kuvaa_valikud():
+    print("\nValikud:")
+    print("1. Kasutaja registreerimine")
+    print("2. Kasutaja autoriseerimine")
+    print("3. Nime või parooli muutmine")
+    print("4. Unustatud parooli taastamine")
+    print("5. Lõpetamine")
+
+def kasutaja_registreerimine(kasutajate_nimekiri):
+    kasutajanimi = input("Sisesta kasutajanimi: ")
+    parooli_valik = input("Kas soovid automaatset parooli (A) või luua ise (I)? ").upper()
+    if parooli_valik == "A":
+        parool = module1.genereeri_parool()
+    elif parooli_valik == "I":
+        parool = module1.käsitsi_parool()
+    else:
+        print("Vale valik!")
+        return
+    module1.registreeri_kasutaja(kasutajanimi, parool, kasutajate_nimekiri)
+
+def kasutaja_autoriseerimine(kasutajate_nimekiri):
+    kasutajanimi = input("Sisesta kasutajanimi: ")
+    parool = input("Sisesta parool: ")
+    if kasutajanimi in kasutajate_nimekiri and kasutajate_nimekiri[kasutajanimi] == parool:
+        print("Autentimine õnnestus!")
+    else:
+        print("Vale kasutajanimi või parool!")
+
+def nime_või_parooli_muutmine():
+    print("Valik veel rakendamata.")
+
+def unustatud_parooli_taastamine():
+    print("Valik veel rakendamata.")
+
+kasutajate_nimekiri = {}
+
+while True:
+    kuvaa_valikud()
+    valik = input("Vali tegevus: ")
+
+    if valik == "1":
+        kasutaja_registreerimine(kasutajate_nimekiri)
+
+    elif valik == "2":
+        kasutaja_autoriseerimine(kasutajate_nimekiri)
+
+    elif valik == "3":
+        nime_või_parooli_muutmine()
+
+    elif valik == "4":
+        unustatud_parooli_taastamine()
+
+    elif valik == "5":
+        break
+
+    else:
+        print("Vale valik!")
